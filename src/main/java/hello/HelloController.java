889e9e00457aa9ead7fff8d44d5fa9520048da71
Amith Nambiar <amith.nmbr@gmail.com>
2016-05-10 19:19:35 +1000

    initial  commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/person")
+    @ResponseBody
+    public Person getPerson() {
+        return new Person("Amith", 18);
+    }
+
+    
+}
